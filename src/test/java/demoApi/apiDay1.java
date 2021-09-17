b262c50a75441d438022d6ec15b77690c9a54008
Uzma <uzmaahsan27@gmail.com>
2021-09-17 19:49:09 -0400

    UpdateApi

--- src/test/java/demoApi/apiDay1.java
+++ src/test/java/demoApi/apiDay1.java
@@ -1,11 +1,23 @@
 package demoApi;
 
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class apiDay1 {
     @Test
-    public void test1(){
-
+    public void test() {
+        String baseurl = "http://3.224.114.109:8000";
+        Response response = RestAssured.given().accept(ContentType.JSON)
+                .when()
+                .get(baseurl + "/api/spartans");
+        System.out.println("response.statusCode() = " + response.statusCode());
+        Assertions.assertEquals(response.statusCode(), 200);
+        response.prettyPrint();
     }
-}
+
+        }
+
+
